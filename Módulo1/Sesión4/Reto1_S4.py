0b5c492bee532820ca386ed909747981ad5d5cb1
IsauraRs <isaurars97@gmail.com>
2020-11-09 22:26:43 -0600

    Se agrega carpeta correspondiente a la sesion 4

---
+++ Módulo1/Sesión4/Reto1_S4.py
@@ -0,0 +1,52 @@
+#Reto 1
+
+#Fecha, nombre y texto de cada comentario
+# Requires the PyMongo package.
+# https://api.mongodb.com/python/current
+
+client = MongoClient('mongodb+srv://Isauraa:bedumongo@clusterforbedu.citlm.mongodb.net/admin?authSource=admin&replicaSet=atlas-y9jsnn-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
+filter={}
+project={
+    'date': 1, 
+    'name': 1, 
+    'text': 1
+}
+
+result = client['sample_mflix']['comments'].find(
+  filter=filter,
+  projection=project
+)
+
+#
+
+# Requires the PyMongo package.
+# https://api.mongodb.com/python/current
+
+client = MongoClient('mongodb+srv://Isauraa:bedumongo@clusterforbedu.citlm.mongodb.net/admin?authSource=admin&replicaSet=atlas-y9jsnn-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
+filter={}
+project={
+    'title': 1, 
+    'cast': 1, 
+    'year': 1
+}
+
+result = client['sample_mflix']['movies'].find(
+  filter=filter,
+  projection=project
+)
+
+# Requires the PyMongo package.
+# https://api.mongodb.com/python/current
+
+client = MongoClient('mongodb+srv://Isauraa:bedumongo@clusterforbedu.citlm.mongodb.net/admin?authSource=admin&replicaSet=atlas-y9jsnn-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
+filter={}
+project={
+    'name': 1, 
+    'password': 1, 
+    '_id': 0
+}
+
+result = client['sample_mflix']['users'].find(
+  filter=filter,
+  projection=project
+)
